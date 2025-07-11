8c0238978b3e97e0f6737c2251399bb604ea5284
soheil <sohylnoor1473@gmail.com>
2025-07-11 20:39:59 +0330

    add some style but not done yet...tommrow we will work on that

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# todo-learning-journey
-Step-by-step journey of building a full-featured To-Do List app using vanilla JavaScript, HTML, and CSS.
+# To-Do Learning Journey 📝
+
+This repository documents my personal journey building a full-featured To-Do List app from scratch using vanilla JavaScript, HTML, and CSS.
+
+## 🌱 Day 1
+- Basic HTML structure (input, button, ul)
+- JS DOM manipulation (add task)
+- Dark Mode toggle using classList
+
+## 🚀 Day 2
+- Added `select` inputs for priority and category
+- Added datetime picker
+- Created task object with full metadata
+- Dynamically built list item including category and due date
--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-const input = document.querySelector("#To-Do")
+const task = document.querySelector("#To-Do")
+const priority = document.querySelector("#priority")
+const category = document.querySelector("#category")
+const time = document.querySelector("#time")
 const button = document.querySelector("#btn")
 const list = document.querySelector("#list")
 
 button.addEventListener("click", () => {
+    if (task.value.trim() === "") {
+        alert("pleas inter your task:");
+        return;
+    }
+
     const li = document.createElement("li")
-    li.textContent = input.value
-    list.appendChild(li)
-    input.value = ""
+    const text = document.createElement("p")
+    const priorityText = document.createElement("span")
+    priorityText.textContent = `Priority:${priority.value}`
+    const categorytext = document.createElement("span")
+    categorytext.textContent = `category:${category.value}`
+    const timetext = document.createElement("span")
+    timetext.textContent = `time:${time.value}`
+    text.textContent = task.value
+    li.appendChild(text)
+    li.appendChild(priorityText);
+    li.appendChild(categorytext);
+    li.appendChild(timetext);
+    list.appendChild(li);
+
+    li.classList.add(`priority-${priority.value}`); // میشه priority-high
+    li.classList.add(`category-${category.value}`); // میشه category-Work
+
+    task.value = ""
+    priority.value = "low"
+    category.value = "Work"
+    time.value = ""
 })
 
 const darkmode = document.querySelector("#toggleDark")
